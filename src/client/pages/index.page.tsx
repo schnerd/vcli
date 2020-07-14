4ba4f7ef80dadafc72eb73fedf9c03787df016d7
David Schnurr <schnurr@gmail.com>
2020-07-13 17:12:07 -0700

    add new favicons

--- src/client/pages/index.page.tsx
+++ src/client/pages/index.page.tsx
@@ -8,7 +8,8 @@ export default function AppContainer() {
       <div className="container">
         <Head>
           <title>vcli</title>
-          <link rel="icon" href="/favicon.ico" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         </Head>
         <Main />
         <style jsx global>{`
