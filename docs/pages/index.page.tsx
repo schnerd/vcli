23996ca1bf7e9fcaa03f7043780d7b5e6a4d8eee
David Schnurr <schnurr@gmail.com>
2020-07-15 22:41:33 -0700

    new hero image

--- docs/pages/index.page.tsx
+++ docs/pages/index.page.tsx
@@ -14,6 +14,7 @@ export default function AppContainer() {
       <Head>
         <title>vcli</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
       </Head>
