863760d641f39e8ddd33d3dd8d68b435895dc936
David Schnurr <schnurr@gmail.com>
2020-07-19 12:27:42 -0700

    canonical tag

--- docs/pages/index.page.tsx
+++ docs/pages/index.page.tsx
@@ -23,6 +23,7 @@ export default function AppContainer() {
         />
         <meta property="og:type" content="website" />
         <meta property="og:image" content="/vcli-og-img.jpg" />
+        <link rel="canonical" href="https://vcli.dev" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
