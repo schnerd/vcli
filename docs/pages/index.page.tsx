ca1fe587cd9c97ca1518620581a2dedbf217b726
David Schnurr <schnurr@gmail.com>
2020-07-19 12:24:36 -0700

    open graph tags

--- docs/pages/index.page.tsx
+++ docs/pages/index.page.tsx
@@ -12,8 +12,17 @@ export default function AppContainer() {
   return (
     <MDXProvider components={mdxComponents}>
       <Head>
-        <title>vcli</title>
+        <title>vcli | Quickly visualize data from the command line</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="og:title" content="vcli | quickly visualize data from the command line" />
+        <meta property="og:site_name" content="vcli" />
+        <meta property="og:url" content="https://vcli.dev/" />
+        <meta
+          property="og:description"
+          content="vcli helps you quickly visualize CSV data from files, APIs, and other programs"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="/vcli-og-img.jpg" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
